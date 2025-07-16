1d25f31342bf47228cbb20daaa928418c602ca7b
Vlada-pixel <vladkadiachenko07@gmail.com>
2025-07-16 10:55:37 +0300

    Rainbow color

---
+++ Assets/Scripts/Rainbow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class Rainbow : MonoBehaviour
+{
+    [SerializeField] private Renderer _myRenderer;
+    private float _hue = 0;
+
+    void Start()
+    {
+        StartCoroutine(ChangeColor());
+    }
+
+    private IEnumerator ChangeColor()
+    {
+        while (true)
+        {
+            _hue += Time.deltaTime * 0.2f;
+            if (_hue > 1f) _hue = 0f;
+
+            Color newColor = Color.HSVToRGB(_hue, 1f, 1f);
+            _myRenderer.sharedMaterial.color = newColor;
+
+            yield return null;
+        }
+    }
+}
